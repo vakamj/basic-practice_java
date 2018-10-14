bcf0cf3e7b92cb0c1dfe4a0f5578a5d44d61de34
Valya Myakisheva <myakisheva.59490@gmail.com>
2018-10-14 19:48:02 +0600

    StringBuilder

---
+++ part_1.java
@@ -0,0 +1,10 @@
+package new_pack;
+
+public class part_1 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("The ");
+        sb.append("first ");
+        sb.append("exercise");
+        System.out.println(sb);
+    }
+}
