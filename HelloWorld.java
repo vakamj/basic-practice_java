6cfb9dc7f2c154245a6bd34e6245587231a6a3b0
Valya Myakisheva <myakisheva.59490@gmail.com>
2018-10-14 18:36:53 +0600

    HW br1

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ package new_pack;
 
     public class HelloWorld {
         public static void main(String[] args) {
-            System.out.println("Hello, World");
+            System.out.println("Hello, World from branch1");
         }
     }
