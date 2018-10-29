933bc7891759ba60d05713b1d27a8bd9234fbc43
Valya Myakisheva <myakisheva.59490@gmail.com>
2018-10-29 11:35:15 +0600

    PairSummater added

---
+++ src/main/java/ioexpertsdotcom/array/PairSummater.java
@@ -0,0 +1,19 @@
+package ioexpertsdotcom.array;
+
+public class PairSummater implements IArraySummater {
+
+    @Override
+    public int sum(int[] array) throws ArraySummaterException {
+        if (array.length % 2 != 0) {
+            throw new ArraySummaterException("message");
+        }
+
+            int [] array2 = new int [];
+            for (int i=0; i < array.length; i++) {
+
+                if ( )
+                    }
+            }
+        return 0;
+    }
+}
