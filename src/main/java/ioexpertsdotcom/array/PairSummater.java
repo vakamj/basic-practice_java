9fb9607798d2fd6367bb91f3c4d73d8442503697
Valya Myakisheva <myakisheva.59490@gmail.com>
2018-10-29 12:00:55 +0600

    max sum of two array elements found

--- src/main/java/ioexpertsdotcom/array/PairSummater.java
+++ src/main/java/ioexpertsdotcom/array/PairSummater.java
@@ -5,15 +5,17 @@ public class PairSummater implements IArraySummater {
     @Override
     public int sum(int[] array) throws ArraySummaterException {
         if (array.length % 2 != 0) {
-            throw new ArraySummaterException("message");
+            throw new ArraySummaterException("\n" +
+                    "array length is not a multiple of two");
         }
-
-            int [] array2 = new int [];
-            for (int i=0; i < array.length; i++) {
-
-                if ( )
-                    }
+        int sumElements = array[0] + array[1];
+        int[] arraySum = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            arraySum[i] = array[2 * i] + array[2 * i + 1];
+            if (sumElements < arraySum[i]) {
+                sumElements = arraySum[i];
             }
-        return 0;
+        }
+        return sumElements;
     }
 }
