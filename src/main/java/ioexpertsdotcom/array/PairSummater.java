17922bd6781de040549520aadae5984776a8b5b3
Valya Myakisheva <myakisheva.59490@gmail.com>
2018-11-03 20:06:16 +0600

    Calculated max sum pairs array

--- src/main/java/ioexpertsdotcom/array/PairSummater.java
+++ src/main/java/ioexpertsdotcom/array/PairSummater.java
@@ -1,19 +1,32 @@
 package ioexpertsdotcom.array;
 
+/**
+ *
+ */
 public class PairSummater implements IArraySummater {
-
+    /**
+     * max amount of pairs array
+     *
+     * @param array array
+     * @return sumElements
+     * @throws ArraySummaterException exception
+     */
     @Override
-    public int sum(int[] array) throws ArraySummaterException {
+    public int sum(final int[] array) throws ArraySummaterException {
         if (array.length % 2 != 0) {
             throw new ArraySummaterException("\n" +
                     "array length is not a multiple of two");
         }
+
+        if (array.length == 0) {
+            throw new ArraySummaterException("\n" +
+                    "empty array");
+        }
+
         int sumElements = array[0] + array[1];
-        int[] arraySum = new int[array.length];
-        for (int i = 0; i < array.length; i++) {
-            arraySum[i] = array[2 * i] + array[2 * i + 1];
-            if (sumElements < arraySum[i]) {
-                sumElements = arraySum[i];
+        for (int i = 0; i < array.length / 2; i++) {
+            if (sumElements < array[2 * i] + array[2 * i + 1]) {
+                sumElements = array[2 * i] + array[2 * i + 1];
             }
         }
         return sumElements;
