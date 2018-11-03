9d0b27c8cdb8d0744b74f6760ac5899f0054763f
Valya Myakisheva <myakisheva.59490@gmail.com>
2018-11-03 20:00:47 +0600

    Interface

--- src/main/java/ioexpertsdotcom/array/IArraySummater.java
+++ src/main/java/ioexpertsdotcom/array/IArraySummater.java
@@ -1,4 +1,14 @@
 package ioexpertsdotcom.array;
 
+/**
+ *
+ */
 public interface IArraySummater {
+    /**
+     * @param array array
+     * @return  sum
+     * @throws ArraySummaterException exception
+     */
+    int sum(int [] array) throws ArraySummaterException;
+
 }
