9b4e4d647eef3d12ddba94b69bb347907b408f4c
Valya Myakisheva <myakisheva.59490@gmail.com>
2018-11-10 20:15:07 +0600

    Tests renamed, javadoc added

---
+++ src/test/java/com/tenExperts/array/SimpleSummaterTest.java
@@ -0,0 +1,31 @@
+package com.tenExperts.array;
+
+
+import com.tenExperts.array.exceptions.ArraySummaterException;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SimpleSummaterTest {
+    /**
+     * sum array with elements
+     *
+     * @throws ArraySummaterException exception
+     */
+    @Test
+    public void sum() throws ArraySummaterException {
+        SimpleSummater summater = new SimpleSummater();
+        assertEquals(5, summater.sum(new int[]{1, 2, 2}));
+    }
+
+    /**
+     * sum empty array
+     *
+     * @throws ArraySummaterException exception
+     */
+    @Test(expected = ArraySummaterException.class)
+    public void sum2() throws ArraySummaterException {
+        SimpleSummater summate = new SimpleSummater();
+        summate.sum(new int[]{});
+    }
+}
