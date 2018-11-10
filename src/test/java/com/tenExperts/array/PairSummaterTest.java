dadba5af8ba63cf5493357de3477f776db65efce
Valya Myakisheva <myakisheva.59490@gmail.com>
2018-11-10 20:14:37 +0600

    Tests renamed, javadoc added

---
+++ src/test/java/com/tenExperts/array/PairSummaterTest.java
@@ -0,0 +1,30 @@
+package com.tenExperts.array;
+
+import com.tenExperts.array.exceptions.ArraySummaterException;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PairSummaterTest {
+    /**
+     * max amount of pairs array with elements
+     *
+     * @throws ArraySummaterException exception
+     */
+    @Test
+    public void normalSum() throws ArraySummaterException {
+        PairSummater summate = new PairSummater();
+        assertEquals(6, summate.sum(new int[]{2, 3, 4, 2}));
+    }
+
+    /**
+     * empty array
+     *
+     * @throws ArraySummaterException exception
+     */
+    @Test(expected = ArraySummaterException.class)
+    public void nullSum() throws ArraySummaterException {
+        PairSummater summate = new PairSummater();
+        summate.sum(new int[]{});
+    }
+}
