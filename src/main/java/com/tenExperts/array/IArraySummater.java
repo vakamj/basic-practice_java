f041d699dc8050c27ae2c93b4983729272d484a2
Valya Myakisheva <myakisheva.59490@gmail.com>
2018-11-10 20:03:12 +0600

    Javadoc for sum, interface added

--- src/main/java/com/tenExperts/array/IArraySummater.java
+++ src/main/java/com/tenExperts/array/IArraySummater.java
@@ -1,10 +1,13 @@
-package ioexpertsdotcom.array;
+package com.tenExperts.array;
+
+import com.tenExperts.array.exceptions.ArraySummaterException;
 
 /**
- *
+ * interface IArraySummater
  */
 public interface IArraySummater {
     /**
+     * method sum
      * @param array array
      * @return  sum
      * @throws ArraySummaterException exception
